5ef9f4cf1b1639804b5379d6a5b19a8718eb36a1
Mark Ortega <mortegaponce27@gmail.com>
2023-09-27 22:31:11 -0500

    app.js: Added paramater to square (accept props). Passing in prop value to square from within Board Component.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,27 @@
 import { useState } from 'react';
 
-function Square(){
-  return <button className="square">1</button>;
+function Square( {propValue} ){
+  // The {} "escapes into Javascript from JSX"
+  return <button className="square">{propValue}</button>;
 }
 // This a component called "Square"
 export default function Board(){
   return(
     <>
       <div className="board-row">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value="1"></Square>
+        <Square value="2"></Square>
+        <Square value="3"></Square>
       </div>
       <div className="board-row">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value="4"></Square>
+        <Square value="5"></Square>
+        <Square value="6"></Square>
       </div>
       <div className="board-row">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value="7"></Square>
+        <Square value="8"></Square>
+        <Square value="9"></Square>
       </div>
     </>
   )
