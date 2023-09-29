b07ae0ec9038e22935c754bafbe2f0d6c7615323
Mark Ortega <mortegaponce27@gmail.com>
2023-09-28 22:43:49 -0500

    Handling state of squares(9) from Board component. Now instead of having each square manage its state, the square will ask the board to update itself (square clicked).

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ export default function Square() {
 ```
 - Export: JavaScript keyword makes this function accessible outside of this file.
 - className="square": Button property or **_prop_**
-- Returning JSX NOTE: Have to enclose the return inside one container (DIV, or <></>)
+- Returning JSX NOTE: Have to enclose the return inside one container (DIV, or **Fragments(<> and </>)**)
 ```
   return (
     <>
@@ -24,4 +24,31 @@ export default function Square() {
 ```
 
 
+Lifting state up: 
+
+Moving state from a child component (useState()) to the parent component.
+In the tutorial, they move the state from a square component in the tic-tae-toe
+game to the board. This allows simpler code to determine who won.
+
+Although this example is not the clearest, another example
+would be if you had two counters on a page. Anytime you click on one button
+you want to increate the counter by one unit. For some reason, the person who
+gave you this project want's you to keep an identical counter on the right side of the page.
+
+So one counter on the far left, and one counter on the far right.
+
+Each time you click on the far left, its value increases by one.
+But now your counter on the right is different from the other one.
+
+Now the user has to click on the other button to get them back into sync....
+
+That defeats the purpose of our site, the user has to perform more work and now 
+has had a bad experience with the UX/UI.
+
+To fix this issue, we can move the state value to a parent component, and pass down the props
+down to both child components through a parameter. NOW our counters will stay in sync!
+
+Another change you'll have to do is pass down a function to the child component, to be able
+to change the state in the parent component... (The reasoning for this is that a component
+can only update it's own state. So a child can't update it's parents state and vice versa).
 
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-function Square( {propValue} ){
+function Square( {propValue, squareClick} ){
   // const[var_to_use, setter_function] = useState(initial_value)
   // Any time a setter is used/called React will rerender the 
   // component(in this case the square) since the state has changed.
@@ -14,7 +14,7 @@ function Square( {propValue} ){
 
   // The {} "escapes into Javascript from JSX"
   return (
-    <button className="square" onClick={handleClick}>{propValue}</button>
+    <button className="square" onClick={onSquareClick}>{propValue}</button>
   )
 }
 ///////////////////////////////////////////////////////////////////////
@@ -22,24 +22,44 @@ function Square( {propValue} ){
 export default function Board(){
 
   const [squares, setSquares] = useState(Array(9).fill(null));
-  
+
+  // This will cause infinite loop when passed down as prop. handleClick(i)(calling the function)
+  function handleClick(i){
+    // Grab a COPY of the array.
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    // Calling the setSquare function will let React
+    // Know that the state of the component has changed.
+    // This will cause a re-render of the components that
+    // use the squares state (Board) as well as its child components.
+    setSquares(nextSquares);
+  }
+
   return(
     <>
       <div className="board-row">
-        <Square value={squares[0]}></Square>
-        <Square value={squares[1]}></Square>
-        <Square value={squares[2]}></Square>
+        {/* Use an arrow function. When the square is clicked, the code after the => will run.*/}
+        <Square value={squares[0]} onSquareClick={() => handleClick(1)}></Square>
+        <Square value={squares[1]} onSquareClick={() => handleClick(2)}></Square>
+        <Square value={squares[2]} onSquareClick={() => handleClick(3)}></Square>
       </div>
       <div className="board-row">
-        <Square value={squares[3]}></Square>
-        <Square value={squares[4]}></Square>
-        <Square value={squares[5]}></Square>
+        <Square value={squares[3]} onSquareClick={() => handleClick(4)}></Square>
+        <Square value={squares[4]} onSquareClick={() => handleClick(5)}></Square>
+        <Square value={squares[5]} onSquareClick={() => handleClick(6)}></Square>
       </div>
       <div className="board-row">
-        <Square value={squares[6]}></Square>
-        <Square value={squares[7]}></Square>
-        <Square value={squares[8]}></Square>
+        <Square value={squares[6]} onSquareClick={() => handleClick(7)}></Square>
+        <Square value={squares[7]} onSquareClick={() => handleClick(8)}></Square>
+        <Square value={squares[8]} onSquareClick={() => handleClick(9)}></Square>
       </div>
     </>
   )
 }
+/*  Now that your state handling is in the Board component, the parent Board 
+component passes props to the child Square components so that they can 
+be displayed correctly. When clicking on a Square, the child Square component 
+now asks the parent Board component to update the state of the board. 
+When the Board’s state changes, both the Board component and every child 
+Square re-renders automatically. 
+Keeping the state of all squares in the Board component will allow it to determine the winner in the future.*/
