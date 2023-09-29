ffa0f9b1f67c52bedf9a88a214fb34a53eec7d31
Mark Ortega <mortegaponce27@gmail.com>
2023-09-28 22:18:30 -0500

    app.js: Lifting state up to parent component(board). Make it easier to determine who won. Managing state from Board Component(parent) vs. 9 individual child components.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,36 +4,41 @@ function Square( {propValue} ){
   // const[var_to_use, setter_function] = useState(initial_value)
   // Any time a setter is used/called React will rerender the 
   // component(in this case the square) since the state has changed.
-  const[value, setValue] = useState(null);
+  // const[value, setValue] = useState(null);
 
-  function handleClick(){
-    // console.log("Clicked");
-    setValue("X");
-  }
+  // function handleClick(){
+  //   // console.log("Clicked");
+  //   setValue("X");
+  // }
+
+
   // The {} "escapes into Javascript from JSX"
   return (
-    <button className="square" onClick={handleClick}>{value}</button>
+    <button className="square" onClick={handleClick}>{propValue}</button>
   )
 }
 ///////////////////////////////////////////////////////////////////////
 // This a component called "Square"
 export default function Board(){
+
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  
   return(
     <>
       <div className="board-row">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[0]}></Square>
+        <Square value={squares[1]}></Square>
+        <Square value={squares[2]}></Square>
       </div>
       <div className="board-row">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[3]}></Square>
+        <Square value={squares[4]}></Square>
+        <Square value={squares[5]}></Square>
       </div>
       <div className="board-row">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[6]}></Square>
+        <Square value={squares[7]}></Square>
+        <Square value={squares[8]}></Square>
       </div>
     </>
   )
